72a1cce8d52e5f218fcef5c6fae9a156cb61b934
Billy Billy <eptgen@gmail.com>
2020-07-10 13:19:38 -0700

    added delta time

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+var timeLastCalled = 0;
 
 function setup() {
-      createCanvas(400, 400);
+	createCanvas(400, 400);
+	timeLastCalled = millis();
 }
 
 function draw() {
+      var timeNow = millis();
+	var dt = timeNow - timeLastCalled;
+	timeLastCalled = timeNow;
       background(220);
       fill(10, 10, 225);
       ellipse(100, 100, 50, 50);
